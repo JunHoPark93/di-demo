0d1ee94e6c828a181613669069f25977df9a9d41
pjh374968@gmail.com <pjh374968@gmail.com>
2018-11-13 20:06:05 +0900

    Add Service config

--- src/main/java/jay/springframework/didemo/config/GreetingServiceConfig.java
+++ src/main/java/jay/springframework/didemo/config/GreetingServiceConfig.java
@@ -1,9 +1,12 @@
 package jay.springframework.didemo.config;
 
 import jay.springframework.didemo.services.GreetingRepository;
+import jay.springframework.didemo.services.GreetingService;
 import jay.springframework.didemo.services.GreetingServiceFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
@@ -11,5 +14,26 @@ public class GreetingServiceConfig {
     @Bean
     GreetingServiceFactory greetingServiceFactory(GreetingRepository repository) {
         return new GreetingServiceFactory(repository);
+    }
+
+    @Bean
+    @Primary
+    @Profile({"default", "en"})
+    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("en");
+    }
+
+    @Bean
+    @Primary
+    @Profile("es")
+    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("es");
+    }
+
+    @Bean
+    @Primary
+    @Profile("de")
+    GreetingService primaryGermanService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("de");
     }
 }
--- src/main/java/jay/springframework/didemo/services/PrimarySpanishGreetingService.java
+++ src/main/java/jay/springframework/didemo/services/PrimarySpanishGreetingService.java
@@ -1,12 +1,5 @@
 package jay.springframework.didemo.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Service
-@Profile("es") // profile deactive primary
-@Primary
 public class PrimarySpanishGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
