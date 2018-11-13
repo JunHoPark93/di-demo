4b8e4c7cb70c1d84905679ce317f0c88b5fe5fbd
pjh374968@gmail.com <pjh374968@gmail.com>
2018-11-13 21:22:10 +0900

    Replace all properties to application.properties since it uses springboot

--- src/main/java/jay/springframework/didemo/config/PropertyConfig.java
+++ src/main/java/jay/springframework/didemo/config/PropertyConfig.java
@@ -2,25 +2,13 @@ package jay.springframework.didemo.config;
 
 import jay.springframework.didemo.examplebeans.FakeDataSource;
 import jay.springframework.didemo.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-    @Autowired
-    Environment env;
 
     @Value("${jay.username}")
     String user;
@@ -43,7 +31,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(env.getProperty("USERNAME")); // system property 사용
+        fakeDataSource.setUser(user); // system property 사용
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
